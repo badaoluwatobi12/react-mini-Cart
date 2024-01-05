dbd864bd9f367916d47a8a40a5e9d8cef659f8ae
Alien <alien@Alien.Alien>
2024-01-05 13:44:24 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.scss'
+import { AppProvider } from "./context";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+  <React.StrictMode>
+       <AppProvider>
+      <BrowserRouter>
+     <App />
+     </BrowserRouter>
+    </AppProvider>
+  </React.StrictMode>,
+)
